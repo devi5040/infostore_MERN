6b0e01f4a34f4a3624a5e2a34ea615bdf13dbf87
devi9113624552 <dpraidola@gmail.com>
2025-01-30 22:03:44 +0530

    done with auth controller

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,150 +1,194 @@
-const mongoose = require('mongoose');
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
-const { validationResult } = require('express-validator');
-const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
+const mongoose = require ('mongoose');
+const User = require ('../models/user');
+const bcrypt = require ('bcryptjs');
+const {validationResult} = require ('express-validator');
+const jwt = require ('jsonwebtoken');
+const crypto = require ('crypto');
 
 //sending emails
-const nodemailer = require('nodemailer');
+const nodemailer = require ('nodemailer');
 // nodemailer.config();
 
 //ethernal email for smtp server
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD,
-    },
+const transporter = nodemailer.createTransport ({
+  host: 'smtp.gmail.com',
+  port: 587,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
 });
 
-async function sendEmail(email, token = '') {
-    await transporter.sendMail({
-        from: 'Deviprasad <dpraimd@gmail.com>',
-        to: email,
-        subject: 'Hello world',
-        html: `<h1>Hello,</h1><p>Thank you for registering to Infostore<p><p>We welcome you to our family</p><div>For any support please contact <a href='mailto:dpraimd@gmail.com'>Support</a></div>Please find the link to reset your password:<a href='http://localhost/8080/auth/forgot-password/${token}'>Link</a>`,
-    });
+async function sendEmail (email, otp = '') {
+  await transporter.sendMail ({
+    from: 'Deviprasad <dpraimd@gmail.com>',
+    to: email,
+    subject: 'Hello world',
+    html: `<h1>Hello,</h1><p>Thank you for registering to Infostore<p><p>We welcome you to our family</p><div>For any support please contact <a href='mailto:dpraimd@gmail.com'>Support</a></div>Token for password retrieval: ${otp}
+    OTP is valid for 10mins`,
+  });
 }
 
 //register route handling
-exports.register = async(req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+exports.register = async (req, res, next) => {
+  const errors = validationResult (req);
+  if (!errors.isEmpty ()) {
+    return res.status (400).json ({errors: errors.array ()});
+  }
+
+  const {email, name, password} = req.body;
+
+  try {
+    // Check if email already exists
+    const existingUser = await User.findOne ({email});
+    if (existingUser) {
+      return res.status (422).json ({
+        message: 'Email already exists, please provide a unique email id.',
+      });
     }
 
-    const { email, name, password } = req.body;
+    // Hash password
+    const hashedPassword = await bcrypt.hash (password, 14); // Improved cost factor
 
-    try {
-        // Check if email already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(422).json({
-                message: 'Email already exists, please provide a unique email id.',
-            });
-        }
+    // Create new user
+    const newUser = new User ({email, name, password: hashedPassword});
+    const result = await newUser.save ();
 
-        // Hash password
-        const hashedPassword = await bcrypt.hash(password, 14); // Improved cost factor
+    // Send email after successful user creation
+    await sendEmail (email);
 
-        // Create new user
-        const newUser = new User({ email, name, password: hashedPassword });
-        const result = await newUser.save();
-
-        // Send email after successful user creation
-        await sendEmail(email);
-
-        // Respond with success
-        res
-            .status(201)
-            .json({ message: 'User has been created successfully', user: result });
-    } catch (err) {
-        res
-            .status(500)
-            .json({ message: 'An error occurred while registering the user.' });
-    }
+    // Respond with success
+    res
+      .status (201)
+      .json ({message: 'User has been created successfully', user: result});
+  } catch (err) {
+    res
+      .status (500)
+      .json ({message: 'An error occurred while registering the user.'});
+  }
 };
 
 //login route handling
-exports.login = async(req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+exports.login = async (req, res, next) => {
+  const errors = validationResult (req);
+  if (!errors.isEmpty ()) {
+    return res.status (400).json ({errors: errors.array ()});
+  }
+  const email = req.body.email;
+  const password = req.body.password;
+  try {
+    const existingUser = await User.findOne ({email: email});
+    //return error if user does not exists
+    if (!existingUser) {
+      return res.status (404).json ({message: 'User does not exist'});
     }
-    const email = req.body.email;
-    const password = req.body.password;
-    try {
-        const existingUser = await User.findOne({ email: email });
-        //return error if user does not exists
-        if (!existingUser) {
-            return res.status(404).json({ message: 'User does not exist' });
-        }
 
-        //check the password if user exists
-        const passwordMatches = await bcrypt.compare(
-            password,
-            existingUser.password
-        );
+    //check the password if user exists
+    const passwordMatches = await bcrypt.compare (
+      password,
+      existingUser.password
+    );
 
-        //return if password didn't match
-        if (!passwordMatches) {
-            return res
-                .status(401)
-                .json({ message: 'Wrong password. Please enter correct password ' });
-        }
-
-        const token = await jwt.sign({ email: existingUser.email, userId: existingUser._id },
-            process.env.JWT_SECRET
-        );
-
-        res.status(200).json({
-            message: 'User logged in successfully',
-            accessToken: token,
-            userId: existingUser._id,
-        });
-    } catch (error) {
-        res.status(500).json({ message: 'An error occured' });
+    //return if password didn't match
+    if (!passwordMatches) {
+      return res
+        .status (401)
+        .json ({message: 'Wrong password. Please enter correct password '});
     }
+
+    const token = await jwt.sign (
+      {email: existingUser.email, userId: existingUser._id},
+      process.env.JWT_SECRET
+    );
+
+    res.status (200).json ({
+      message: 'User logged in successfully',
+      accessToken: token,
+      userId: existingUser._id,
+    });
+  } catch (error) {
+    res.status (500).json ({message: 'An error occured'});
+  }
 };
 
 //mail the password reset token
-exports.getAccessLink = async(req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ error: errors.array() });
+exports.getAccessOtp = async (req, res, next) => {
+  const errors = validationResult (req);
+  if (!errors.isEmpty ()) {
+    return res.status (400).json ({error: errors.array ()});
+  }
+  const email = req.body.email;
+  try {
+    const token = crypto.randomInt (100000, 1000000);
+    const user = await User.findOne ({email: email});
+    if (!user) {
+      return res.status (404).json ({
+        message: 'User not available with provided email id',
+      });
     }
-    const email = req.body.email;
-    try {
-        crypto.randomBytes(32, async(err, buffer) => {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Some internal error has occured while creating token',
-                });
-            }
-            const token = buffer.toString('hex');
-            const user = await User.findOne({ email: req.body.email });
-            if (!user) {
-                return res.status(404).json({
-                    message: 'User not available with provided email id',
-                });
-            }
-            const expirationTime = Date.now() + 60 * 10 * 1000;
-            user.resetToken = token;
-            user.tokenExpiration = expirationTime;
-            await user.save();
-            await sendEmail(req.body.email, token);
-            res
-                .status(200)
-                .json({ message: 'Mail sent to the user with access link' });
-        });
-    } catch (error) {
-        res.status(500).json({ message: 'Some error occured' });
-    }
+    const expirationTime = Date.now () + 60 * 10 * 1000;
+    user.resetToken = token;
+    user.tokenExpiration = expirationTime;
+    await user.save ();
+    await sendEmail (email, token);
+    res.status (200).json ({message: 'Mail sent to the user with OTP'});
+  } catch (error) {
+    res.status (500).json ({message: 'Some error occured'});
+  }
 };
 
-//forgot-password route handling
-exports.forgotPassword = (req, res, next) => {
-    console.log('forgotPassword');
+//verify-otp route handling
+exports.verifyOtp = async (req, res, next) => {
+  const errors = validationResult (req);
+  if (!errors.isEmpty ()) {
+    return res.status (400).json ({error: errors.array ()});
+  }
+  const otp = req.body.otp;
+  const email = req.body.email;
+  try {
+    const user = await User.findOne ({
+      email: email,
+    });
+    if (!user) {
+      return res.status (404).json ({message: 'User not found'});
+    }
+    const userOtp = user.resetToken;
+    const expirationTime = user.tokenExpiration;
+    if (otp !== userOtp || expirationTime <= Date.now ()) {
+      return res.status (401).json ({message: 'Invalid otp'});
+    }
+    user.resetToken = undefined;
+    await user.save ();
+    res.status (200).json ({message: 'Otp has been verified'});
+  } catch (error) {
+    res.status (500).json ({message: 'Some internal error has been occured'});
+  }
+};
+
+exports.changePassword = async (req, res, next) => {
+  const errors = validationResult (req);
+  if (!errors.isEmpty ()) {
+    return res.status (400).json ({error: errors.array ()});
+  }
+  const email = req.body.email;
+  const newPassword = req.body.password;
+  try {
+    const user = await User.findOne ({email: email});
+    if (!user) {
+      return res.status (404).json ({message: 'User not found'});
+    }
+    const expirationTime = user.tokenExpiration;
+    if (expirationTime <= Date.now () || expirationTime === undefined) {
+      return res
+        .status (401)
+        .json ({message: 'Time exceeded please try again'});
+    }
+    const hashedPassword = await bcrypt.hash (newPassword, 12);
+    user.password = hashedPassword;
+    user.tokenExpiration = undefined;
+    await user.save ();
+    res.status (200).json ({message: 'Password changed successfully'});
+  } catch (error) {
+    res.status (500).json ({message: 'Some internal error has been occured'});
+  }
 };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,59 +1,86 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controller/auth');
-const { body } = require('express-validator');
+const express = require ('express');
+const router = express.Router ();
+const authController = require ('../controller/auth');
+const {body} = require ('express-validator');
 
 //Route for register
-router.post(
-    '/register', [
-        body('email')
-        .trim()
-        .isEmail()
-        .notEmpty()
-        .withMessage('Email id is not valid'),
-        body('name')
-        .trim()
-        .notEmpty()
-        .isLength({ min: 3 })
-        .withMessage('Entered name is invalid provide valid name'),
-        body('password')
-        .notEmpty()
-        .trim()
-        .isLength({ min: 8 })
-        .withMessage('Password should contain at least 8 characters'),
-    ],
-    authController.register
+router.post (
+  '/register',
+  [
+    body ('email')
+      .trim ()
+      .isEmail ()
+      .notEmpty ()
+      .withMessage ('Email id is not valid'),
+    body ('name')
+      .trim ()
+      .notEmpty ()
+      .isLength ({min: 3})
+      .withMessage ('Entered name is invalid provide valid name'),
+    body ('password')
+      .notEmpty ()
+      .trim ()
+      .isLength ({min: 8})
+      .withMessage ('Password should contain at least 8 characters'),
+  ],
+  authController.register
 );
 
 //Route for login
-router.post(
-    '/login', [
-        body('email')
-        .trim()
-        .isEmail()
-        .notEmpty()
-        .withMessage('Email id is not valid'),
-        body('password')
-        .notEmpty()
-        .trim()
-        .isLength({ min: 8 })
-        .withMessage('Password should contain at least 8 characters'),
-    ],
-    authController.login
+router.post (
+  '/login',
+  [
+    body ('email')
+      .trim ()
+      .isEmail ()
+      .notEmpty ()
+      .withMessage ('Email id is not valid'),
+    body ('password')
+      .notEmpty ()
+      .trim ()
+      .isLength ({min: 8})
+      .withMessage ('Password should contain at least 8 characters'),
+  ],
+  authController.login
 );
 
 //route for getting link for forgot password on mail
-router.post(
-    '/forgot-password',
-    body('email')
-    .trim()
-    .isEmail()
-    .notEmpty()
-    .withMessage('Email id is not valid'),
-    authController.getAccessLink
+router.post (
+  '/get-otp',
+  body ('email')
+    .trim ()
+    .isEmail ()
+    .notEmpty ()
+    .withMessage ('Email id is not valid'),
+  authController.getAccessOtp
 );
 
-//Route for forgot-password
-router.post('/forgot-password/:token');
+//Route for verifying otp
+router.post (
+  '/verify-otp',
+  body ('email')
+    .trim ()
+    .isEmail ()
+    .notEmpty ()
+    .withMessage ('Email id is not valid'),
+  authController.verifyOtp
+);
+
+router.post (
+  '/update-password',
+  [
+    body ('email')
+      .trim ()
+      .isEmail ()
+      .notEmpty ()
+      .withMessage ('Email id is not valid'),
+    body ('password')
+      .notEmpty ()
+      .trim ()
+      .isLength ({min: 8})
+      .withMessage ('Password should contain at least 8 characters'),
+  ],
+  authController.changePassword
+);
 
 module.exports = router;
