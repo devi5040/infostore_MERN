6fd14c673dda5786a17b91cb65c794759d594f1f
devi9113624552 <dpraidola@gmail.com>
2025-02-17 19:09:57 +0530

    added education card

---
+++ infostore-frontend/src/components/ItemCard/EducationCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Button from '../Button/Button';
+
+function EducationCard( { education } ) {
+    console.log( education );
+
+    return (
+        <div className='border border-gray-300 shadow-md rounded-md p-10 text-secondary'>
+            <h2 className='text-center font-bold text-xl'>{ education.education }</h2>
+            <div className='flex flex-col space-y-2'>
+                <div className='flex items-center'>
+                    <h3 className='text-lg font-semibold'>Institue Name:</h3>
+                    <p className=' mx-2'>{ education.institute }</p>
+                </div>
+                <div className='flex items-center'>
+                    <h3 className='text-lg font-semibold'>Marks:</h3>
+                    <p className='mx-2'>{ education.marks }%</p>
+                </div>
+                <div className='flex items-center'>
+                    <h3 className='text-lg font-semibold'>Achievements:</h3>
+                    <p className='mx-2'>{ education.achievements }</p>
+                </div>
+                <div className='flex items-center'>
+                    <h3 className='text-lg font-semibold'>Other Details:</h3>
+                    <p className='mx-2'>{ education.otherDetails }</p>
+                </div>
+                <div className='flex justify-between'>
+                    <Button>Edit</Button>
+                    <button className='bg-red-500 text-primary w-[75px] py-1 rounded-md my-2 cursor-pointer border border-red-500 hover:bg-primary hover:text-red-500 duration-300'>Delete</button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default EducationCard
--- infostore-frontend/src/pages/Education.jsx
+++ infostore-frontend/src/pages/Education.jsx
@@ -1,6 +1,9 @@
 import React, { Fragment, useState } from 'react'
 import EducationAdd from '../components/education/EducationAdd';
 import AddItemButton from '../components/Button/AddItemButton';
+import ItemCard from '../components/ItemCard/ItemCard';
+import EducationCard from '../components/ItemCard/EducationCard';
+import Paginator from '../components/Paginator/Paginator';
 
 let TOTAL_EDUCATION_DETAILS = 0;
 const TOTAL_ITEMS_PER_PAGE = 3;
@@ -8,6 +11,32 @@ const TOTAL_ITEMS_PER_PAGE = 3;
 function Education() {
     const [isEditing, setIsEditing] = useState( false );
     const [isAdding, setIsAdding] = useState( false );
+    const [currentPage, setCurrentPage] = useState( 1 );
+
+    const education_details = [
+        {
+            _id: '67b336d16b70f93ef35d8b5e',
+            education: 'SSLC',
+            institute: 'VHS',
+            marks: '92.64',
+            achievements: '-',
+            otherDetails: '-',
+            userId: '679a64dcb2440f3edcfc21fe',
+            __v: 0,
+        },
+        {
+            _id: '67b336e16b70f93ef35d8b61',
+            education: 'PUC',
+            institute: 'VC',
+            marks: '81.33',
+            achievements: '-',
+            otherDetails: '-',
+            userId: '679a64dcb2440f3edcfc21fe',
+            __v: 0,
+        },
+    ];
+
+    TOTAL_EDUCATION_DETAILS = 7;
 
     const educationAddHandler = () => {
         setIsAdding( true );
@@ -21,6 +50,11 @@ function Education() {
         <Fragment>
             { isAdding && <EducationAdd closeModalHandler={ educationAddCloseModal } /> }
             <AddItemButton btnCaption='Add education details' isEditing={ educationAddHandler } />
+            <Paginator currentPage={ currentPage } onChangePage={ setCurrentPage } lastPage={ Math.ceil( TOTAL_EDUCATION_DETAILS / TOTAL_ITEMS_PER_PAGE ) }>
+                { education_details.map( ( education ) => {
+                    return <EducationCard education={ education } key={ education._id } />
+                } ) }
+            </Paginator>
         </Fragment>
     )
 }
