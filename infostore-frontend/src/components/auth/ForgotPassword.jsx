96996d2c9d39453629f292e63a787496c02e1c63
devi9113624552 <dpraidola@gmail.com>
2025-02-18 18:10:38 +0530

    Added Forgot Password

---
+++ infostore-frontend/src/components/auth/ForgotPassword.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import Modal from '../modal/Modal'
+import TextInput from '../Form-input/TextInput'
+
+function ForgotPassword( { closeModal } ) {
+    const [isOtpSent, setIsOtpSent] = useState( false );
+
+    const getOtpHandler = () => {
+        setIsOtpSent( true )
+    }
+    return (
+        <Modal closeModal={ closeModal } modalHeading='Change Your Password'>
+            <form action="" className={ `${ !isOtpSent ? 'h-[280px]' : 'my-10' }` }>
+                <TextInput label='Enter your Email ID to receive OTP' field='email' placeholder=' ' required />
+                <button type="submit" className='w-full py-2 bg-secondary text-primary rounded-md shadow border border-secondary cursor-pointer hover:bg-white hover:text-secondary' onClick={ getOtpHandler }>Get OTP</button>
+            </form>
+            { isOtpSent && <form action="">
+                <TextInput label='Enter the OTP' field='otp' placeholder=' ' required />
+                <button type="submit" className='w-full py-2 bg-secondary text-primary rounded-md shadow border border-secondary cursor-pointer hover:bg-white hover:text-secondary'>Verify OTP</button>
+            </form> }
+        </Modal>
+    )
+}
+
+export default ForgotPassword
